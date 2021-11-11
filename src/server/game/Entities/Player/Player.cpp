12bef6cbcde095a6b4b7176f2b7270ea3c99bd83
itswicky <blaindes51@gmail.com>
2021-11-10 23:45:39 -0600

    Core/Talent: Correct logic for only allowing one specialization

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -13121,19 +13121,17 @@ void Player::LearnTalent(uint32 talentId, uint32 talentRank)
 
     // Check if it is exclusive with another talent
     if (talentInfo->addToSpellBook >= 2)
-    {
         // loop through all talents
         for (uint32 i = 0; i < sTalentStore.GetNumRows(); i++)
             if (TalentEntry const* tmpTalent = sTalentStore.LookupEntry(i))
                 // check if talent has a flags value of 2 or greater
                 if (tmpTalent->addToSpellBook >= 2)
                     // check if we already know a spell with flags value 2 or greater
-                    if (HasSpell(tmpTalent->RankID[0]))
+                    if (HasAura(tmpTalent->RankID[0]))
                     {
                         GetSession()->SendNotification("You may only have one specialization selected at a time.");
                         return;
                     }
-    }
 
     // xinef: check amount of points spent in current talent tree
     // xinef: be smart and quick
@@ -13150,7 +13148,7 @@ void Player::LearnTalent(uint32 talentId, uint32 talentRank)
     }
 
     // xinef: we do not have enough talent points to add talent of this tier - custom check for 1 point per tier
-    if (spentPoints < (talentInfo->Row * MAX_TALENT_RANK - 4))
+    if (spentPoints < (talentInfo->Row * (MAX_TALENT_RANK - 4)))
         return;
 
     // custom check for only 1 talent per tier
